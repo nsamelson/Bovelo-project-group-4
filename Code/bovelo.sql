90b403a2dea2cc058dd078c53a9f3fd0a5544015
17338 <47603034+17338@users.noreply.github.com>
2021-02-17 13:33:39 +0100

    Delete error

--- Code/bovelo.sql
+++ Code/bovelo.sql
@@ -7,7 +7,7 @@ INSERT INTO Part VALUES(5,"Guidon 26","A4",80,2,"OXY SOUDURE",3);
 INSERT INTO Part VALUES(1,"Guidon 28","A5",90,4,"OXY SOUDURE",4);
 
 
-CREATE TABLE Bike(id_bike INTEGER NOT NULL,BikeType TEXT NOT NULL,StateBuilding BOOLEAN,StateReadyBuilding BOOLEAN,BuildingTime INTEGER NOT NULL,Quantity INTEGER NOT NULL).
+CREATE TABLE Bike(id_bike INTEGER NOT NULL,BikeType TEXT NOT NULL,StateBuilding BOOLEAN,StateReadyBuilding BOOLEAN,BuildingTime INTEGER NOT NULL,Quantity INTEGER NOT NULL)
 INSERT INTO Bike VALUES(1,"City 26",0,0,3,10);
 INSERT INTO Bike VALUES(2,"City 28",0,0,3,10);
 INSERT INTO Bike VALUES(3,"Explorer 26",0,0,4,8);
@@ -21,9 +21,9 @@ INSERT INTO OrderBike VALUES(2,0,NULL,"Processing","rue de l'expedition");
 INSERT INTO OrderBike VALUES(3,1,NULL,"ReadyToBuild","rue de l'expedition");
 INSERT INTO OrderBike VALUES(4,1,NULL,"ReadyToSend","rue de l'expedition");
 
-CREATE TABLE User(id_user INTEGER NOT NULL,StateUser INTEGER,FirstName TEXT,LastName TEXT,UserLogin TEXT,UserPassword TEXT);
-INSERT INTO User VALUES(1,0,"Jean","Dupond","205556","motdepassenonsecurise");
-INSERT INTO User VALUES(2,2,"Eric","Dubois","205557","motdepassenonsecurise");
-INSERT INTO User VALUES(3,1,"Camille","Duruisseau","205558","motdepassenonsecurise");
-INSERT INTO User VALUES(4,1,"Luc","Dufleuve","205559","motdepassenonsecurise");
-INSERT INTO User VALUES(5,1,"Herve","Dujardin","205560","motdepassenonsecurise");
+CREATE TABLE User_value(id_user INTEGER NOT NULL,StateUser INTEGER,FirstName TEXT,LastName TEXT,UserLogin TEXT,UserPassword TEXT);
+INSERT INTO User_value VALUES(1,0,"Jean","Dupond","205556","motdepassenonsecurise");
+INSERT INTO User_value VALUES(2,2,"Eric","Dubois","205557","motdepassenonsecurise");
+INSERT INTO User_value VALUES(3,1,"Camille","Duruisseau","205558","motdepassenonsecurise");
+INSERT INTO User_value VALUES(4,1,"Luc","Dufleuve","205559","motdepassenonsecurise");
+INSERT INTO User_value VALUES(5,1,"Herve","Dujardin","205560","motdepassenonsecurise");
