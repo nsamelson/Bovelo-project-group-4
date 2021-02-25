156cece2ef807994678bc24c45cb2c610c6bd963
Antoine SCAVINER <20521@ecam.be>
2021-02-25 11:30:56 +0100

    "try fiw pipelines - add program.cs"

---
+++ Code/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Bovelo
+{
+    static class Program
+    {
+
+        private static App app = new App();
+        [STAThread]
+        
+        static void Main(string[] args)
+        {
+            Application.Run(new Login());
+
+         }
+    }
+}
