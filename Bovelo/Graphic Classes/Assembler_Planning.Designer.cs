d4e58952edc2e811c22b01c980bff8d79855e00a
AmineAousji <aousjimedamine9@gmail.com>
2021-03-24 19:10:26 +0100

    AB#524 du code perdue lors du dernier commit à cause de fusion

--- Bovelo/Graphic Classes/Assembler_Planning.Designer.cs
+++ Bovelo/Graphic Classes/Assembler_Planning.Designer.cs
@@ -32,16 +32,6 @@ namespace Bovelo
             this.button7 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Bike_Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Color = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Bike_Part = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.timeToBuild = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Modified_by = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Started = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.Finsihed = new System.Windows.Forms.DataGridViewButtonColumn();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
@@ -54,6 +44,17 @@ namespace Bovelo
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Bike_Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Color = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Bike_Part = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timeToBuild = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modified_by = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Started = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Finsihed = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.set_on_new = new System.Windows.Forms.DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -98,6 +99,7 @@ namespace Bovelo
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(53)))), ((int)(((byte)(64)))));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -110,95 +112,16 @@ namespace Bovelo
             this.timeToBuild,
             this.Modified_by,
             this.Started,
-            this.Finsihed});
+            this.Finsihed,
+            this.set_on_new});
             this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.dataGridView1.Location = new System.Drawing.Point(248, 140);
+            this.dataGridView1.Location = new System.Drawing.Point(209, 144);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(5);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(725, 328);
+            this.dataGridView1.Size = new System.Drawing.Size(800, 328);
             this.dataGridView1.TabIndex = 3;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "Order Detail Id:";
-            this.Column1.MinimumWidth = 30;
-            this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
-            // 
-            // Bike_Type
-            // 
-            this.Bike_Type.FillWeight = 80.54144F;
-            this.Bike_Type.HeaderText = "Bike Type :";
-            this.Bike_Type.MinimumWidth = 6;
-            this.Bike_Type.Name = "Bike_Type";
-            this.Bike_Type.ReadOnly = true;
-            // 
-            // Color
-            // 
-            this.Color.FillWeight = 80.54144F;
-            this.Color.HeaderText = "Bike Size :";
-            this.Color.MinimumWidth = 6;
-            this.Color.Name = "Color";
-            this.Color.ReadOnly = true;
-            // 
-            // Column2
-            // 
-            this.Column2.FillWeight = 80.54144F;
-            this.Column2.HeaderText = "Bike Color :";
-            this.Column2.MinimumWidth = 6;
-            this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
-            // 
-            // Bike_Part
-            // 
-            this.Bike_Part.FillWeight = 80.54144F;
-            this.Bike_Part.HeaderText = "ID | Name | Location | Quantity";
-            this.Bike_Part.MinimumWidth = 6;
-            this.Bike_Part.Name = "Bike_Part";
-            this.Bike_Part.ReadOnly = true;
-            // 
-            // Price
-            // 
-            this.Price.FillWeight = 80.54144F;
-            this.Price.HeaderText = "Bike Status :";
-            this.Price.MinimumWidth = 6;
-            this.Price.Name = "Price";
-            this.Price.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            // 
-            // timeToBuild
-            // 
-            this.timeToBuild.FillWeight = 80.54144F;
-            this.timeToBuild.HeaderText = "Week :";
-            this.timeToBuild.MinimumWidth = 6;
-            this.timeToBuild.Name = "timeToBuild";
-            // 
-            // Modified_by
-            // 
-            this.Modified_by.HeaderText = "Assembled by :";
-            this.Modified_by.MinimumWidth = 6;
-            this.Modified_by.Name = "Modified_by";
-            // 
-            // Started
-            // 
-            this.Started.HeaderText = "Started at: ";
-            this.Started.Name = "Started";
-            this.Started.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.Started.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            this.Started.Text = "set on active";
-            this.Started.ToolTipText = "set on active";
-            this.Started.UseColumnTextForButtonValue = true;
-            // 
-            // Finsihed
-            // 
-            this.Finsihed.HeaderText = "Finished at:";
-            this.Finsihed.Name = "Finsihed";
-            this.Finsihed.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.Finsihed.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            this.Finsihed.Text = "set on close";
-            this.Finsihed.ToolTipText = "set on close";
-            this.Finsihed.UseColumnTextForButtonValue = true;
             // 
             // button2
             // 
@@ -217,10 +140,10 @@ namespace Bovelo
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(980, 326);
+            this.button3.Location = new System.Drawing.Point(1022, 326);
             this.button3.Margin = new System.Windows.Forms.Padding(5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(170, 42);
+            this.button3.Size = new System.Drawing.Size(128, 37);
             this.button3.TabIndex = 6;
             this.button3.Text = "Get Planning";
             this.button3.UseVisualStyleBackColor = true;
@@ -231,7 +154,7 @@ namespace Bovelo
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(977, 242);
+            this.label2.Location = new System.Drawing.Point(1019, 242);
             this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 16);
@@ -242,10 +165,10 @@ namespace Bovelo
             // 
             this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(980, 273);
+            this.comboBox1.Location = new System.Drawing.Point(1022, 273);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(170, 29);
+            this.comboBox1.Size = new System.Drawing.Size(128, 29);
             this.comboBox1.TabIndex = 14;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -361,6 +284,94 @@ namespace Bovelo
             this.label4.TabIndex = 1;
             this.label4.Text = "release 3.0";
             // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Order Detail Id:";
+            this.Column1.MinimumWidth = 30;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Bike_Type
+            // 
+            this.Bike_Type.FillWeight = 80.54144F;
+            this.Bike_Type.HeaderText = "Bike Type :";
+            this.Bike_Type.MinimumWidth = 6;
+            this.Bike_Type.Name = "Bike_Type";
+            this.Bike_Type.ReadOnly = true;
+            // 
+            // Color
+            // 
+            this.Color.FillWeight = 80.54144F;
+            this.Color.HeaderText = "Bike Size :";
+            this.Color.MinimumWidth = 6;
+            this.Color.Name = "Color";
+            this.Color.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 80.54144F;
+            this.Column2.HeaderText = "Bike Color :";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Bike_Part
+            // 
+            this.Bike_Part.FillWeight = 80.54144F;
+            this.Bike_Part.HeaderText = "ID | Name | Location | Quantity";
+            this.Bike_Part.MinimumWidth = 6;
+            this.Bike_Part.Name = "Bike_Part";
+            this.Bike_Part.ReadOnly = true;
+            // 
+            // Price
+            // 
+            this.Price.FillWeight = 80.54144F;
+            this.Price.HeaderText = "Bike Status :";
+            this.Price.MinimumWidth = 6;
+            this.Price.Name = "Price";
+            this.Price.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // timeToBuild
+            // 
+            this.timeToBuild.FillWeight = 80.54144F;
+            this.timeToBuild.HeaderText = "Week :";
+            this.timeToBuild.MinimumWidth = 6;
+            this.timeToBuild.Name = "timeToBuild";
+            // 
+            // Modified_by
+            // 
+            this.Modified_by.HeaderText = "Assembled by :";
+            this.Modified_by.MinimumWidth = 6;
+            this.Modified_by.Name = "Modified_by";
+            // 
+            // Started
+            // 
+            this.Started.HeaderText = "Started at: ";
+            this.Started.Name = "Started";
+            this.Started.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Started.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Started.Text = "set on active";
+            this.Started.ToolTipText = "set on active";
+            this.Started.UseColumnTextForButtonValue = true;
+            // 
+            // Finsihed
+            // 
+            this.Finsihed.HeaderText = "Finished at:";
+            this.Finsihed.Name = "Finsihed";
+            this.Finsihed.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Finsihed.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Finsihed.Text = "set on close";
+            this.Finsihed.ToolTipText = "set on close";
+            this.Finsihed.UseColumnTextForButtonValue = true;
+            // 
+            // set_on_new
+            // 
+            this.set_on_new.HeaderText = "Set on new";
+            this.set_on_new.Name = "set_on_new";
+            this.set_on_new.Text = "Reset on new";
+            this.set_on_new.ToolTipText = "Reset on new";
+            this.set_on_new.UseColumnTextForButtonValue = true;
+            // 
             // Assembler_Planning
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
@@ -421,5 +432,6 @@ namespace Bovelo
         private System.Windows.Forms.DataGridViewTextBoxColumn Modified_by;
         private System.Windows.Forms.DataGridViewButtonColumn Started;
         private System.Windows.Forms.DataGridViewButtonColumn Finsihed;
+        private System.Windows.Forms.DataGridViewButtonColumn set_on_new;
     }
 }
