8d24f7565dc2f8e67cb3344b0017a798fb0ec651
Matthieu Faget <20517@ecam.be>
2021-03-27 15:13:07 +0100

    graphic Manager provider orders modif and button in Main home

--- Bovelo/Graphic Classes/MainHome.cs
+++ Bovelo/Graphic Classes/MainHome.cs
@@ -120,8 +120,7 @@ namespace Bovelo
 
         private void button5_Click_1(object sender, EventArgs e)
         {
-            
-            if (_currentUser.login == "Manager" )
+            if (_currentUser.userType["Production Manager"] == true ) // if (user is a manager) is true ... 
             {
                 this.Hide();
                 Manager_MainHome mmh = new Manager_MainHome(_currentUser);// create new window
