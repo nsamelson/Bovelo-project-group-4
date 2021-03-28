2226281d8c9b030c238f0270b5b31fedc02345c4
Wasanico <salocin.sam@gmail.com>
2021-03-28 17:15:32 +0200

    AB#659 : minor change in OrderBike

--- Bovelo/Classes/OrderBike.cs
+++ Bovelo/Classes/OrderBike.cs
@@ -16,21 +16,18 @@ namespace Bovelo
         public int totalPrice;
         public DateTime orderDate=DateTime.Now;
         public DateTime shippingDate=DateTime.Now;
-        public List<List<string>> orderDetail; //Details of the order : [id,Client_Name,Bike_Type,Bike_Color,Bike_Size,Quantity,Price,Order_Time]
-        public List<Bike> bikeList;
-        private List<BikeModel> _bikeModels;
+        //public List<List<string>> orderDetail; //Details of the order : [id,Client_Name,Bike_Type,Bike_Color,Bike_Size,Quantity,Price,Order_Time]
+        public List<Bike> bikeList = new List<Bike>();
 
         public OrderBike(string clientName, List<List<string>> orderDetail, int id,DateTime orderDate,DateTime shippingDate,int totalPrice,List<BikeModel> bikeModels)
         {
             this.clientName = clientName;
-            this.orderDetail = orderDetail;
             this.orderId = id;
             this.orderDate = orderDate;
             this.shippingDate = shippingDate;
             this.totalPrice = totalPrice;
-            this._bikeModels = bikeModels;
-            this.bikeList = getBikeList();
             this.isReadyToShip = getOrderState();
+            setBikeList(bikeModels, orderDetail);
         }
 
         public float getTotalBuildingTime()
@@ -42,9 +39,12 @@ namespace Bovelo
             }
             return hours / 60;
         }
-        private List<Bike> getBikeList()
+        public List<Bike> getBikeList()
         {
-            var bikes = new List<Bike>();
+            return bikeList;
+        }
+        private void setBikeList(List<BikeModel> bikeModels,List<List<string>> orderDetail)
+        {
             foreach(var elem in orderDetail)
             {
                 int id = Int32.Parse(elem[0]);
@@ -53,12 +53,12 @@ namespace Bovelo
                 string color = elem[3];
                 //int price = Int32.Parse(elem[4]);
 
-                BikeModel model = _bikeModels.FirstOrDefault(x => x.Color == color && x.Size == size && x.Type == type);//gets the specific model
+                BikeModel model = bikeModels.FirstOrDefault(x => x.Color == color && x.Size == size && x.Type == type);//gets the specific model
                 Bike bike = new Bike(id, model);
                 bike.setNewState(elem[5]);
-                bikes.Add(bike );//adds a corresponding Bike
+                bikeList.Add(bike );//adds a corresponding Bike
             }
-            return bikes;
+            
         }
         
         public bool getOrderState()//needs to be tested
